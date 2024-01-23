f9403077f0959e218a4a99c03939db6d54596b69
pedroffd <pedro.alcarin@gmail.com>
2024-01-23 20:50:43 -0300

    basic styling for input

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
+    function goalInputHandler(enteredText) {
+        console.log(enteredText)
+    }
+
+    function addGoalHandler() {}
+
     return (
         <View style={styles.appContainer}>
             <View style={styles.inputContainer}>
                 <TextInput
                     placeholder="Your course goal!"
+                    onChangeText={goalInputHandler}
                     styles={styles.textInput}
                 />
-                <Button title="Add Goal" />
+                <Button title="Add Goal" onPress={addGoalHandler} />
             </View>
-            <View>
+            <View style={styles.goalsContainer}>
                 <Text>List of goals...</Text>
             </View>
         </View>
@@ -19,17 +26,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
     appContainer: {
-        padding: 50,
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16,
     },
     inputContainer: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
     },
     textInput: {
         borderWidth: 1,
         borderColor: '#cccccc',
-        width: '80%',
+        width: '70%',
         marginRight: 8,
         padding: 8,
+    },
+    goalsContainer: {
+        flex: 4,
     },
 })
